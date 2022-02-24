088cb7fdc2fb68c2948239b3101545782cd260f8
Mojasagwe <78989798+Mojasagwe@users.noreply.github.com>
2022-02-24 13:15:26 +0200

    Add files via upload

---
+++ ZenTech/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        double num1 =0, num2 =0;
+        double sum = addition(num1,num2);
+
+        System.out.println(sum);
+    }
+
+
+    public static double addition(double num1, double num2){
+
+      return num1+num2;
+    }
+}
+
+
